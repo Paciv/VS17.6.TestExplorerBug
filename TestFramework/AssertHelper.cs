7fb0dec33bc8471853de1a721519dbd0505138be
Paul BARRY DELONGCHAMPS <paul.barry-delongchamps@sgcib.com>
2023-06-23 17:13:39 +0900

    Initial commit

---
+++ TestFramework/AssertHelper.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace TestFramework
+{
+    public class AssertHelper
+    {
+        public static void IsItReallyTrue(bool condition) => Assert.That(condition, Is.True);
+    }
+}
---
+++ TestProject/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using TestFramework;
+
+namespace TestLibrary
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Theory]
+        public void Test1(bool condition)
+        {
+            AssertHelper.IsItReallyTrue(condition);
+        }
+    }
+}
